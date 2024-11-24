1ba0e29c21b808386e6e5070d33f12e66fed31fa
Mattea <mattea.sax@gmail.com>
2024-11-24 11:13:58 +0100

    added not found page with animated floating div

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,28 @@
 /* Not-found Page */
 
+import { useNavigate } from "react-router-dom";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="not-found py-5 flex flex-col justify-center items-center cursor-pointer"
+      onClick={() => {
+        navigate("/");
+      }}
+    >
+      <div className="not-found__content bg-s500 p-10 flex flex-col gap-5 cursor-default color-s400">
+        <h1 className="heading heading--primary ">404 | Not Found</h1>
+        <p className="body body--primary">
+          Sorry, we couldn't fint the page you were looking for!{" "}
+        </p>
+        <p className="body body--primary">
+          Click on any Speedy Snail to go back to homepage 🚀
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
