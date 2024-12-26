40b84f08234069ddd722d9a2d6ab14d54d57219c
Mattea <mattea.sax@gmail.com>
2024-12-26 10:57:45 +0100

    added some more to tailwind config because font color was not showing in deployed version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./src/components/**/*.{html,js,ts,jsx,tsx}",
+    "./src/pages/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
