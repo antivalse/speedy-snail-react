93af5b2d5bae82579a46539b6980a221f4420a7b
Mattea <mattea.sax@gmail.com>
2024-12-26 10:07:01 +0100

    added a fallback on scroll to div function so that it scrolls to top if the div isnt found

--- src/utils/helpers/scrollToDiv.ts
+++ src/utils/helpers/scrollToDiv.ts
@@ -1,10 +1,17 @@
 /* Scroll into view function */
 
-const scrollToDiv = (elementId: string) => {
-  const div = document.getElementById(elementId);
-  div?.scrollIntoView({
-    behavior: "smooth",
-  });
+const scrollToDiv = (elementId?: string) => {
+  if (elementId) {
+    const div = document.getElementById(elementId);
+    div?.scrollIntoView({
+      behavior: "smooth",
+    });
+  } else {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
 };
 
 export default scrollToDiv;
